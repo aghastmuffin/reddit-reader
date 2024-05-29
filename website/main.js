e8f8f142e378f1e04ebbea8b59d93dacaf28b296
aghastmuffin <78246775+aghastmuffin@users.noreply.github.com>
2024-05-28 22:29:29 -0700

    website v2?

---
+++ website/main.js
@@ -0,0 +1,17 @@
+function dwnld(url) {
+    document.getElementById('my_iframe').src = url;
+      const paragraph = document.createElement('p');
+    paragraph.textContent = "Give it a couple seconeds, but if that didn't work click";
+
+    // Create a new anchor element
+    const anchor = document.createElement('a');
+    anchor.href = "https://github.com/aghastmuffin/reddit-reader/archive/refs/heads/main.zip";
+    anchor.textContent = " here!";
+    anchor.referrerPolicy = "no-referrer"; // Sets the referrer policy to 'no-referrer'
+
+    // Append the anchor to the paragraph
+    paragraph.appendChild(anchor);
+
+    // Append the paragraph to the body of the document
+    document.body.appendChild(paragraph);
+};
